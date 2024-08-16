7fdf51150d910c72cf552713c8ec576e475adbf8
AmaanShamir <amaanshamirshaikh9767@gmail.com>
2024-08-16 14:13:20 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="amaanshaikh12@gmail.com"
+var accountPassword="12"
+accountCity="jaipur"
+/*not preffered to declare variablr like this but it can be used */
+let accountState ;
+accountEmail="sadsdfdfg"
+accountPassword ="123"
+//accountId=-=2// Not allowed 
+accountCity= "haryana"
+console.log(accountId);
+
+
+/*prefer  not to use var
+because of issue in block code and functiomnal  s\c
+*/
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState])

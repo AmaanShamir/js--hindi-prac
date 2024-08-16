cbee32c8de357cc3528409aa9708fb90da8819e1
AmaanShamir <amaanshamirshaikh9767@gmail.com>
2024-08-16 14:16:49 +0000

    repdatatypes

---
+++ 01_basics/repdatatypes.js
@@ -0,0 +1,17 @@
+"use strict" ; // treat all code as newer version of JS noimp but a good practise
+//alert("hello") we ARE using node Js not browser 
+console.log(3+ 3)// code redability should be high 
+
+console.log("amaan")
+let name= "Amaan "// string
+let age =18 // int 
+let IsloggedIn= true // boolean 
+// num=>2 to power 53 available in memory address 
+// bigibt
+// string can be used with "", or single quote
+// boolean =>true / fales
+// null => standalone value represetiontation of empty value 
+// undefined => not defined 
+// symbol => unique
+// obj
+console.log(typeof "Amaan ") 

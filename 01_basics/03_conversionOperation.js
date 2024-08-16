47e10fcc1a208484a56e766316740963be416f14
AmaanShamir <amaanshamirshaikh9767@gmail.com>
2024-08-16 14:48:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score= false // returns 1 for true and 0 for false
+console.log(typeof score);
+console.log(typeof (score));// sameas above 
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);// returns not a num NaN
+// "33"=>33 
+// 33abc=> NaN 
+// true = 1
+
+let IsloggedIn = 1
+let booleanIsloggedIn= Boolean(IsloggedIn)
+console.log( booleanIsloggedIn);
+// 1=>truew ; 0= false
+//"amaaan"= true
+// " "= false
+
+let somenumber=33
+let stringnumber= String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)

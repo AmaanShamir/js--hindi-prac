b03b548fe12871c2ff7014ba860f1534bf1a2670
AmaanShamir <amaanshamirshaikh9767@gmail.com>
2024-09-01 09:01:29 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "amaan"
+const repocount = 50;
+console.log(name + repocount + "value");
+console.log(``)//backticks location [0][0]
+//string interpolation 
+console.log(`helo myname is ${name} and my repo count is ${repocount}`);//$ for variable
+
+const gamename= new String ('amaan_shaik_h');// new use obj 
+
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('s'));
+
+
+
+const newString = gamename.substring (3, 8);
+console.log(newString);
+
+
+
+const anotherString = gamename.slice(-12, 8);
+console.log(anotherString);
+
+
+console.log(gamename.split('a'));
+
+
+
+
+
+
+
+ 
